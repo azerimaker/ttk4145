b2620ed8f40dce8f3339137c1d97a0188f2ba1ef
Magnus Kirø <magnuskiro@gmail.com>
2012-02-10 13:06:45 +0100

    fib complete, mus handle input sanitation

--- ex3/fib.adb
+++ ex3/fib.adb
@@ -2,24 +2,21 @@ with Ada.Text_IO, Ada.Integer_Text_IO;
 use Ada.Text_IO, Ada.Integer_Text_IO;
 
 procedure fibonacci is
-	n:INTEGER;
+    function fib (N:Integer) return Integer is
+    begin
+        if n < 2 then
+            return n;
+        elsif n = 2 then
+            return 1;
+        elsif n mod 2 = 0 then
+            return (fib( (n/2-1) ) + fib( (n/2+1) )) * fib(n/2);
+        else
+            return fib(n/2)**2 + fib(n/2+1)**2 ;
+        end if;
+    end fib;
+    N:INTEGER;
 begin
-	Put("Enter fib num: ");
-	Get(n);
-	fib(n);
+    Put("enter n");
+    Get(N);
+    Put(fib(N));
 end fibonacci;
-
-
-
-procedure fib (n) is
-begin
-    if n < 2 then
-        Put(n);
-    elsif n = 2 then
-        Put(1);
-    elsif n mod 2 = 0 then
-        Put( (fib(n/2-1) + fib(n/2+1)) * fib(n/2) );
-    else
-        Put( fib(n/2)**2 + fib(n/2+1)**2 );
-    end if;
-end fib;
