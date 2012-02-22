1de2c2f8720223f0542924563ce34e3d09b037a1
Jan Alexander Stormark Bremnes <janbremnes@gmail.com>
2012-02-22 16:20:03 +0100

    fixed some errors in channel.go

--- ex4/part1/channel.go
+++ ex4/part1/channel.go
@@ -11,20 +11,20 @@ var t int
 // reads an int from the channel and multiplies it with 2
 func calcSum(c chan int) {
 	no := <- c
-	runtime.Gosched()
+	//runtime.Gosched()
 	if no == 1 {
-		fmt.Println(no, "bottle of beer on the wall! Go to the store, buy some more!")
+		fmt.Println(no, "bottle of beer on the wall!",no,"bottle of beer!")
 		t = 99
-		fmt.Println(t, "bottles of beer on the wall!\n")
+		fmt.Println("Go to the store, buy some more!",t,"bottles of beer on the wall!\n")
 	} else {
 		runtime.Gosched()
-		fmt.Println(no, "bottles of beer on the wall",no,"bottles of beer!\nTake one down, pass it around",no-1,"bottles of beer on the wall!\n")
+		fmt.Println(no, "bottles of beer on the wall!",no,"bottles of beer!\nTake one down, pass it around",no-1,"bottles of beer on the wall!\n")
 	}
 }
 
 // increments a counter which it puts on the channel
 func count(c chan int) {
-	runtime.Gosched()
+	//runtime.Gosched()
 	c <- t
 	t--
 }
@@ -35,7 +35,7 @@ func main() {
 	for i := 0; i < 99; i++ {
 		go count(c)
 		go calcSum(c)
-		time.Sleep(1*1e8)
+		time.Sleep(1*1e7)
 	}
 }
 	
