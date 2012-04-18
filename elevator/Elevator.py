55b4def76ab89228d691001ea088251b18e2de09
Magnus Kirø <magnuskiro@gmail.com>
2012-04-18 20:59:27 +0200

    added variables in the elevator object

--- elevator/Elevator.py
+++ elevator/Elevator.py
@@ -3,4 +3,8 @@ __author__ = 'kiro'
 class Elevator():
 
     def __init__(self):
-        self.
+        self.IP = ""
+        self.status = ""
+        self.manager = "F"
+        self.lastAlive = "time of last message from this elevator"
+        self.age = "initialization time of this elevator."
