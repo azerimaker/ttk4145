dd42b86a270ec95cfea95b5276f8c21f4e24e472
Magnus Kirø <magnuskiro@gmail.com>
2012-04-18 20:38:28 +0200

    added functionality for manager check

--- elevator/Main.py
+++ elevator/Main.py
@@ -1,18 +1,41 @@
+import threading
+
 __author__ = 'kiro'
 
 # to be the top most file, where we call everything else from.
 
 class Main():
+    #self.Manager
+    #sself.Communicator
+    #self.DataStore
+    #self.MessageHandler
+    #self.ElevatorControl
+
+    def __init__(self):
+        print "main init"
+        isManagerCheck()
 
     # see if there is still a manager
 
     def setManagerState(self, state):
         self.managerState = state
 
-    # thread
-        # if self.state[0] != "T":
-            # communicator.send("T-----")
-            # createManager()
 
     def createManager(self):
         print "we are now the manager, Wohoo!"
+
+
+class isManagerCheck( threading.Thread ):
+    # thread
+    # if self.state[0] != "T":
+    # communicator.send("T-----")
+    # createManager()
+
+    def __init__(self, main):
+        super.super(isManagerCheck, self).__init__()
+        self.main = main
+
+    def run(self):
+        print "checking manager status"
+        if self.main.managerState[0]!="T":
+            self.main.createManager()
