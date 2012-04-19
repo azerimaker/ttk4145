74f600c83d8240bd08ed1019de6fe1e3f04329dc
Jan Alexander S. Bremnes <janbremnes@gmail.com>
2012-04-19 22:36:31 +0200

    logic stuff... pure chaos atm

--- elevator/control.py
+++ elevator/control.py
@@ -16,50 +16,93 @@ example entry: 'elevator1': '18:31:37'
 ELEVATORS = {}
                         
 from time import time
+import constants
 
-'''
-go through job list to see if any available jobs in same direction
-as the elevator is headed, if so, add them to current job list
-'''
-def get_jobs():
-    if DIRECTION == 'UP':
-        for job in GOING_UP:
-            if job >= current_floor:
-                CURRENT_JOBS.add(job)                 
-    elif DIRECTION == 'DOWN':
-        for job in GOING_DOWN:
-            if job <= current_floor:
-                CURRENT_JOBS.add(job)         
-'''
-when passing a floor, check if there are people waiting on the next floor
-if there are, raise flag so the elevator stops when reaching the floor
-'''
-def check_next_floor():
-    if CURRENT_FLOOR+1 in CURRENT_JOBS:
-        STOP_NEXT = True
-        
-# check to see if other elevators have problems
-def check_others:
-    for key, value in ELEVATORS:
+
+class Control:
+
+    '''
+    Initialize the control object
+    '''
+    def __init__(self):
+        self.job_list = [[0 for x in range(NO_FLOORS)] for x in range(2)] # direction is one dimension, no. floors the other
+        self.peers = {} # store peer objects, with IP as key       
+        self.dispatcher = False
+        self.networking = "" 
+        self.age = time()
+
+    '''
+    setup elevator to be ready
+    '''
+    def setup(messages, networking):
+        self.networking = networking
+        networking.do_stuff()
+        # spawn networking thread
+        # this thread sets up all the necessary connections
+        # and listens for incoming messages for a set time period
+        for message in messages:
+            last_alive = time()
+            peers[message.id] = Peer(message.id, message.status, message.dispatcher, last_alive, message.age)
+        networking.set_peers(peers) # give networking thread the list of peers so that it can update them
+        networking.broadcast_existence()
+        start()
+           
+    
+    '''
+    when passing a floor, check if there are people waiting on the next floor
+    if there are, raise flag so the elevator stops when reaching the floor
+    '''
+    def check_next_floor():
+        if CURRENT_FLOOR+1 in CURRENT_JOBS:
+            STOP_NEXT = True
+   
+    '''        
+    check to see if other elevators have problems
+    if an elevator has timed out, delete from list
+    '''
+    def check_others():
         currentTime = time()
-        if (currentTime - value) < 3:
-            continue
-        else:
-            # TODO:
-            # elevator[key] is dead! DO SOMETHING!
-            # add the dead elevators job list to private list
-            # send private list to other elevators
-            
-'''
-listen for other elevators
-add these to system overview
-'''            
-def listen():
-    # TODO
-    
+        for peer_id, peer in peers.iteritems():
+            if (currentTime - peer.last_alive) < TIMEOUT:
+                continue
+            else:
+                del peers[peer.id]
+                if peer.dispatcher:
+                    new_dispatcher()
+                
+    '''
+    listen for other elevators
+    add these to system overview
+    '''            
+    def listen():
+        # TODO: networking class takes care of this
+        
 
-'''
-Start the elevator
-'''
-def start():
+    '''
+    Method to decide which elevator becomes dispatcher
+    if dispatcher has died. Oldest elevator is dispatcher
+    '''
+    def new_dispatcher():
+        elder = self
+        for peer_id, peer in peers.iteritems():
+            if peer.age > elder.age:
+                elder = peer
+        if elder == self:
+            become_dispatcher()
+
     
+    '''
+    Turns this elevator into dispatcher
+    '''
+    def become_dispatcher():
+        dispatcher = Dispatcher()
+        # TODO: set relevant datafields in dispatcher object
+        # TODO: stop control loop, give control to dispatcher
+        
+    
+    
+    '''
+    Start the elevator
+    '''
+    def start():
+        
--- elevator/initialize.py
+++ elevator/initialize.py
@@ -6,9 +6,14 @@ Initializes the elevator, broadcasts it's existence on
 the network, and starts the elevator control program
 '''
 def main():
-    initalize_datastructures()
-    broadcast()
-    control.start() # elevator is now ready to run
+    networking = Network()
+    messages = networking.init()
+    if len(messages) == 0:  # no other elevators, this one becomes dispatcher
+        dispatcher = Dispatcher()
+        dispatcher.start(networking)
+    else:
+        controller = Control()
+        controller.setup(messages, networking)
     
 '''
 Notifies all online elevators that a new one has
@@ -18,9 +23,6 @@ def broadcast():
     # TODO: broadcast "I'm here" on network
     control.listen() # listen for other elevators
     
-def initialize_datastructure():
-    # these are in a file of their own
-    # so perhaps no need for this method?
     
   
     
