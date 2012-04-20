00a522eb29c411e669dc63ff0a2362dd20399fcc
Magnus Kirø <magnuskiro@gmail.com>
2012-04-20 22:19:37 +0200

    more improvements

--- elevator/Controller.py
+++ elevator/Controller.py
@@ -20,13 +20,12 @@ class Controller:
         self.status = ""
         self.elevator = Elevator() # this object should handle the interface with the driver and keep track of elevator state
         
-        
-    '''        
-    check to see if other elevators have problems
-    if an elevator has timed out, delete from list
-    if dispatcher dead, create new dispatcher
-    '''
     def check_others(self):
+        '''
+        check to see if other elevators have problems
+        if an elevator has timed out, delete from list
+        if dispatcher dead, create new dispatcher
+        '''
         currentTime = time()
         dead_peers = []
         dispatcher_dead = False
@@ -42,13 +41,12 @@ class Controller:
         elif dispatcher_dead:
             self.find_new_dispatcher(dead_peers)
             print "Dispatcher is dead, need to find new"
-            
-    
-    '''
-    Method to decide which elevator becomes dispatcher
-    if dispatcher has died. Highest IP (id) is dispatcher
-    '''
+
     def find_new_dispatcher(self, dead_peers):
+        '''
+        Method to decide which elevator becomes dispatcher
+        if dispatcher has died. Highest IP (id) is dispatcher
+        '''
         elder = self
         for peer_id, peer in self.peerList.iteritems():
             if peer in dead_peers:
@@ -59,21 +57,19 @@ class Controller:
             self.become_dispatcher(dead_peers)
         else:
             print "New dispatcher is ", elder.id
-            
 
-    '''
-    Turns this elevator into dispatcher
-    '''
     def become_dispatcher(self, dead_peers):
+        '''
+        Turns this elevator into dispatcher
+        '''
         self.dispatcher = True
         print "This elevator is now dispatcher.\nRedistributing jobs of dead peers"
         self.redistribute_jobs(dead_peers)
 
-
-    '''
-    setup elevator to be ready
-    '''
     def setup(self, messages):
+        '''
+        setup elevator to be ready
+        '''
 #        networking.do_stuff()
         # spawn networking thread
         # this thread sets up all the necessary connections
@@ -83,7 +79,6 @@ class Controller:
             self.peerList[message.id] = Peer(message.id, message.status, message.dispatcher, last_alive)
 #        networking.set_peers(peers) # give networking thread the list of peers so that it can update them
 #        networking.broadcast_existence()
-        self.start()
 
     def newOrder(self, message):
         # TODO
@@ -109,30 +104,28 @@ class Controller:
 ##### DISPATCHER METHODS ########
 #################################
 
-    '''
-    Finds the elevator best suited for a job
-    '''     
+
     def find_best_suited(self, floor, direction):
+        '''
+        Finds the elevator best suited for a job
+        '''
         best_score = -1
         best_suited = ""
         for peer_id, peer in self.peerList.iteritems():
             if peer.is_obstructed or peer.is_stopped:
                 continue
             else:
-                score = get_score(peer, floor, direction)
+                score = self.get_score(peer, floor, direction)
                 if score < best_score:
                     best_score = score
                     best_suited = peer
-        if best_score == 0:
-            add_to_backlog(job)
-                    
         return best_suited
-    
-    ''' 
-    Gives elevator a score based on how suited it is for the job.
-    A score of 1 is best 
-    '''
+
     def get_score(self, peer, floor, direction):
+        '''
+        Gives elevator a score based on how suited it is for the job.
+        A score of 1 is best
+        '''
         # TODO: FIGURE OUT HOW BEST TO GIVE A SCORE! THIS IS PROBABLY THE WORST PART LOGIC-WISE!
         score = 0.0
         scaling_factor = 1/NO_FLOORS
@@ -161,11 +154,12 @@ class Controller:
         else:
             return score
         
-    '''
-    This method takes a dead elevator and redistributes its jobs
-    to the other elevators that are still online
-    '''
+
     def redistribute_jobs(self, dead_peers):
+        '''
+        This method takes a dead elevator and redistributes its jobs
+        to the other elevators that are still online
+        '''
         # TODO: takes in a dead peer, redistributes its jobs to the ones still online
         for peer in dead_peers:
             for direction in range(2):
@@ -174,11 +168,12 @@ class Controller:
                         self.dispatch_job(floor,direction)
 
     
-    '''
-    Takes a job and finds an elevator to give it to
-    When job list is updated, broadcast to all
-    '''    
+
     def dispatch_job(self, floor, direction):
+        '''
+        Takes a job and finds an elevator to give it to
+        When job list is updated, broadcast to all
+        '''
         # if job is not in list of jobs, find elevator to take it
         if not self.orderList[direction][floor]:
             peer = self.find_best_suited(floor, direction)
